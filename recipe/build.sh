52569648a2ff844581db2b3c58aff16db6307402
Yuchen Fan <fyc0624@gmail.com>
2020-06-04 10:59:42 -0500

    init

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr
-pushd ${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr > /dev/null 2>&1
+mkdir -p ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/usr
+pushd ${PREFIX}/powerpc64le-conda_cos7-linux-gnu/sysroot/usr > /dev/null 2>&1
 cp -Rf "${SRC_DIR}"/binary/* .
